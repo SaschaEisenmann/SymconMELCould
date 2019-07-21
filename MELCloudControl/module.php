822f603090fe843cf3d7af468e7db41403a0677a
Sascha Eisenmann <sascha@eisenmann.it>
2019-07-21 13:41:21 +0200

    Implemented login

--- MELCloudControl/module.php
+++ MELCloudControl/module.php
@@ -17,5 +17,66 @@ class MELCloudControl extends IPSModule
 
     public function Sync()
     {
+        if ($this->ReadPropertyString('Token') == '') {
+            $this->CreateToken();
+        }
+    }
+
+    private function CreateToken()
+    {
+        $url = "https://app.melcloud.com/Mitsubishi.Wifi.Client/Login/ClientLogin";
+
+        $params = array();
+        $params['Email'] = $this->ReadPropertyString('Email');
+        $params['password'] = $this->ReadPropertyString('Password');
+        $params['AppVersion'] = "1.7.1.0";
+
+        $headers = array();
+        $headers[] = "Accept: application/json";
+
+        $result = $this->Request($url, 'POST', $params, $headers);
+
+        if (isset($result["LoginData"]) && isset($result["LoginData"]["ContextKey"])) {
+            IPS_SetProperty($this->InstanceID, 'Token', $result["LoginData"]["ContextKey"]);
+            IPS_ApplyChanges($this->InstanceID);
+            $this->SetStatus(102);
+        } else {
+            $this->SetStatus(201);
+        }
+    }
+
+    public function Request($url, $method, $params = array(), $headers = array())
+    {
+        $client = curl_init($url);
+        curl_setopt($client, CURLOPT_CUSTOMREQUEST, $method);
+//        curl_setopt($client, CURLOPT_SSL_VERIFYHOST, 0);
+//        curl_setopt($client, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+//        curl_setopt($client, CURLOPT_USERAGENT, 'SymconBotvac');
+//        curl_setopt($client, CURLOPT_CONNECTTIMEOUT, 5);
+//        curl_setopt($client, CURLOPT_TIMEOUT, 5);
+
+        if ($method == 'POST') {
+            curl_setopt($client, CURLOPT_POSTFIELDS, http_build_query($params));
+        }
+
+        curl_setopt($client, CURLOPT_HTTPHEADER, $headers);
+
+
+        $result = curl_exec($client);
+        $status = curl_getinfo($client, CURLINFO_HTTP_CODE);
+
+        curl_close($client);
+
+        if ($status == '0') {
+            $this->SetStatus(201);
+            return false;
+        } elseif ($status != '200' && $status != '201') {
+            IPS_LogMessage("SymconMELCloud", "Response invalid. Code $status");
+            $this->SetStatus(201);
+            return false;
+        } else {
+            return json_decode($result, true);
+        }
     }
 }
